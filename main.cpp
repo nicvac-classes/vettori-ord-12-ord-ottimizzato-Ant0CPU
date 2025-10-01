c40a30b1ba72b8f432eff7bcea07a02295046b00
Ant0CPU <antonio.bruno@marconibari.edu.it>
2025-10-01 17:20:02 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,59 @@
 #include <iostream>
+#include <cmath>
+#include <ctime>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+
+    srand(time(0));
+
+    int n, i(0), j, t;
+    bool scambi(true);
+
+    do {
+        cout<<"Inserire il numero di cifre da generare"<<endl;
+        cin>>n;
+    } while (n<=0);
+
+    vector <int> R(n);
+    while (i<=n-1) {
+
+        R[i]=rand() % (n*10+1);
+        i=i+1;
+    }
+
+    i=0;
+    while (i<=n-1 && scambi) {
+        scambi=false;
+
+        j=0;
+        while (j<(n-1)-i) {
+           
+            if (R[j]>R[j+1]) {
+                
+                t=R[j];
+                R[j]=R[j+1];
+                R[j+1]=t;
+
+                scambi=true;
+
+            } 
+            else
+            {
+                scambi=false;
+            }
+            j=j+1;
+        }
+        i=i+1;
+    }
+
+    cout<<"R: ";
+    for (i=0; i<=n-1; i++) {
+        
+        cout<<R[i]<<"; ";
+
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
